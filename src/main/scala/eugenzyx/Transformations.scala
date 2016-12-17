ac0b18ca65b249144b0e07f6ea27db80c3db34f1
Eugen Prokhorenko <eugen.prokhorenko@gmail.com>
2016-12-17 17:34:03 +0200

    turn functions into lambdas, add some tests

--- src/main/scala/eugenzyx/Transformations.scala
+++ src/main/scala/eugenzyx/Transformations.scala
@@ -6,35 +6,32 @@ import org.apache.spark.sql.Row
 
 trait Transformations {
   type TweetSummary = (String, String, String)
+  type VertexWithCount = (VertexId, Int)
 
-  def tweetToIdTextPairRDD(tuple: TweetSummary): (Long, String) = tuple match {
+  def tweetToIdTextPairRDD: TweetSummary => (Long, String) = tweetSummary => tweetSummary match {
     case (id, text, _) => (id.toLong, text)
   }
 
-  def responseToIdTextPairRDD(tuple: TweetSummary): (Long, String) = tuple match {
+  def responseToIdTextPairRDD: TweetSummary => (Long, String) = tweetSummary => tweetSummary match {
     case (_, _, inReplyToStatusId) => (inReplyToStatusId.toLong, "")
   }
 
-  def extractEdges(tuple: TweetSummary): Edge[String] = tuple match {
+  def extractEdges: TweetSummary => Edge[String] = tweetSummary => tweetSummary match {
     case (id, _, inReplyToStatusId) => Edge(id.toLong, inReplyToStatusId.toLong, "Replies")
   }
 
-  def toTweetSummary(row: Row): TweetSummary = (row.getAs[String]("id"), row.getAs[String]("text"), row.getAs[String]("inReplyToStatusId"))
+  def toTweetSummary: Row => TweetSummary = row => (row.getAs[String]("id"), row.getAs[String]("text"), row.getAs[String]("inReplyToStatusId"))
 
-  def onlyValidRecords(tweetSummary: TweetSummary): Boolean = tweetSummary match {
+  def onlyValidRecords: TweetSummary => Boolean = tweetSummary => tweetSummary match {
     case (id, _, inReplyToStatusId) =>
       List(id, inReplyToStatusId)
         .foldLeft(true)((start, value) =>
             start && value.forall(_.isDigit)) // filter out corrupted tuples
   }
 
-  def getCount(tuple: (VertexId, Int)): Int = tuple match {
-    case (_, count) => count
-  }
+  def getCount: VertexWithCount => Int = tuple => tuple match { case (_, count) => count }
 
-  def getIds(tuple: (VertexId, Int)): Long = tuple match {
-    case (id, _) => id
-  }
+  def getIds: VertexWithCount => VertexId = tuple => tuple match { case (id, _) => id }
 
   val descending = false
 }
